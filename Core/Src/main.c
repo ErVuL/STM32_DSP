e9a52c3241a5c924152bf38ede43c3263f9b0833
ErVuL <theo.bertet@gmail.com>
2021-01-17 19:14:43 +0100

    git  test

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -115,7 +115,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_DMA_Init();
+  MX_DMA_Init();qgit push
   MX_I2S2_Init();
   MX_USB_DEVICE_Init();
   MX_CRC_Init();
